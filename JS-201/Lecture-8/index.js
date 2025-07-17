d6ef0ceb176058c25a4ae8b65e1010e8ac390103
Harsh2004R <115461808+Harsh2004R@users.noreply.github.com>
2025-07-17 13:01:59 +0530

    Notion link added for Lecture -8

--- JS-201/Lecture-8/index.js
+++ JS-201/Lecture-8/index.js
@@ -1,4 +1,8 @@
+// Asynchronous Javascript - I
 // just promise and it's .then and .catch methods clearification lecture...
+// reffere this link to understand more about Asynchronous Javascript - I
+//  ----- >     https://www.notion.so/vivmagarwal/Async-Javascript-Class-9-10-11-4847102033934073aa05e2a57bfaf193
+
 
 function returnPromise(num) {
     let promise = new Promise(function (res, rej) {
